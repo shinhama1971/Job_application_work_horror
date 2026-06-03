e8c7f5551dcba360c8d7f213d39f9fcc65fc3d27
shinhama1971 <shinhama0508@gmail.com>
2026-06-03 20:41:49 +0900

    値をテストで変更しました

--- Job_application_work_horror/TitleScene.cpp
+++ Job_application_work_horror/TitleScene.cpp
@@ -19,7 +19,7 @@ void TitleScene::Init()
 {
 	Texture2D* pt = Game::GetInstance()->AddObject<Texture2D>();
 	pt->SetTexture("assets/texture/background1.png");//画像を指定
-	pt->SetPosition(0.0f, 0.0f, 1.0f);//位置を指定
+	pt->SetPosition(0.0f, 0.0f, 0.0f);//位置を指定
 	pt->SetRotation(0.0f, 0.0f, 0.0f);//角度を指定
 	pt->SetScale(1280.0f, 720.0f, 0.0f);//大きさを指定
 	m_MySceneObjects.emplace_back(pt);
@@ -30,6 +30,7 @@ void TitleScene::Init()
 	pt1->SetRotation(0.0f, 0.0f, 0.0f);//角度を指定
 	pt1->SetScale(580.0f, 360.0f, 0.0f);//大きさを指定
 	m_MySceneObjects.emplace_back(pt1);
+	//これを繰り返してオブジェクトを追加していく
 }
 
 // 更新
