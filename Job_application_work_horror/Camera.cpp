78eab4c2d147f4ab7da2a5fce3826a1f584dd177
shinhama1971 <shinhama0508@gmail.com>
2026-07-08 15:48:18 +0900

    appu

--- Job_application_work_horror/Camera.cpp
+++ Job_application_work_horror/Camera.cpp
@@ -7,130 +7,175 @@ using namespace DirectX::SimpleMath;
 
 void Camera::Init()
 {
-	m_Position = Vector3(0.0f, 20.0f, -50.0f);
-	m_Target = Vector3(0.0f, 0.0f, 0.0f);
-	m_CameraDirection = 3.14f;
-	m_CameraPitch = 0.0f;
+    m_Position = Vector3(0.0f, 20.0f, -50.0f);
+    m_Target = Vector3(0.0f, 0.0f, 0.0f);
 
-	// マウスカーソルを非表示にする
-	ShowCursor(FALSE);
+    m_CameraDirection = 3.14f;
+    m_CameraPitch = 0.0f;
+
+    m_MouseLookEnable = true;
+    m_FirstMouse = true;
+
+    ShowCursor(FALSE);
 }
 
 void Camera::Update()
 {
-	// マウス移動で視線制御
-	POINT mousePos;
-	GetCursorPos(&mousePos);
+    // ESCでマウス視点ON/OFF
+    if (Input::GetKeyTrigger(VK_ESCAPE))
+    {
+        m_MouseLookEnable = !m_MouseLookEnable;
+        m_FirstMouse = true;
 
-	if (m_FirstMouse)
-	{
-		m_LastMousePos = mousePos;
-		m_FirstMouse = false;
-	}
-	else
-	{
-		float dx = (float)(mousePos.x - m_LastMousePos.x);
-		float dy = (float)(mousePos.y - m_LastMousePos.y);
+        if (m_MouseLookEnable)
+        {
+            ShowCursor(FALSE);
+        }
+        else
+        {
+            ShowCursor(TRUE);
+        }
+    }
 
-		float sensitivity = 0.003f;
+    if (m_MouseLookEnable)
+    {
+        POINT mousePos;
+        GetCursorPos(&mousePos);
 
-		m_CameraDirection += dx * sensitivity;
-		m_CameraPitch += dy * sensitivity;
+        if (m_FirstMouse)
+        {
+            m_LastMousePos = mousePos;
+            m_FirstMouse = false;
+        }
+        else
+        {
+            float dx = (float)(mousePos.x - m_LastMousePos.x);
+            float dy = (float)(mousePos.y - m_LastMousePos.y);
 
-		const float maxPitch = 1.2f;
-		const float minPitch = -1.2f;
-		if (m_CameraPitch > maxPitch) m_CameraPitch = maxPitch;
-		if (m_CameraPitch < minPitch) m_CameraPitch = minPitch;
-	}
+            float sensitivity = 0.003f;
 
-	// マウスカーソルをウィンドウの中央に戻す
-	int screenWidth = Application::GetWidth();
-	int screenHeight = Application::GetHeight();
-	POINT screenCenter = { screenWidth / 2, screenHeight / 2 };
+            m_CameraDirection += dx * sensitivity;
+            m_CameraPitch += dy * sensitivity;
 
-	// ウィンドウの座標に変換
-	HWND hWnd = GetActiveWindow();
-	if (hWnd)
-	{
-		ClientToScreen(hWnd, &screenCenter);
-		SetCursorPos(screenCenter.x, screenCenter.y);
-		m_LastMousePos = screenCenter;
-	}
+            const float maxPitch = 1.2f;
+            const float minPitch = -1.2f;
 
-	// 左右キーでカメラ回転（Yaw）
-	if (Input::GetKeyPress(VK_LEFT))
-	{
-		m_CameraDirection += 0.05f;
-	}
-	if (Input::GetKeyPress(VK_RIGHT))
-	{
-		m_CameraDirection -= 0.05f;
-	}
+            if (m_CameraPitch > maxPitch) m_CameraPitch = maxPitch;
+            if (m_CameraPitch < minPitch) m_CameraPitch = minPitch;
+        }
 
-	// 上下キーでカメラ上下（Pitch）
-	const float pitchStep = 0.03f;
-	if (Input::GetKeyPress(VK_UP))
-	{
-		m_CameraPitch += pitchStep;
-	}
-	if (Input::GetKeyPress(VK_DOWN))
-	{
-		m_CameraPitch -= pitchStep;
-	}
+        // マウスカーソルをウィンドウ中央へ戻す
+        int screenWidth = Application::GetWidth();
+        int screenHeight = Application::GetHeight();
+
+        POINT screenCenter =
+        {
+            screenWidth / 2,
+            screenHeight / 2
+        };
+
+        HWND hWnd = GetActiveWindow();
+
+        if (hWnd)
+        {
+            ClientToScreen(hWnd, &screenCenter);
+            SetCursorPos(screenCenter.x, screenCenter.y);
+            m_LastMousePos = screenCenter;
+        }
+    }
+
+    // デバッグ用：矢印キーでも視点操作
+    if (Input::GetKeyPress(VK_LEFT))
+    {
+        m_CameraDirection += 0.05f;
+    }
+
+    if (Input::GetKeyPress(VK_RIGHT))
+    {
+        m_CameraDirection -= 0.05f;
+    }
+
+    const float pitchStep = 0.03f;
+
+    if (Input::GetKeyPress(VK_UP))
+    {
+        m_CameraPitch += pitchStep;
+    }
+
+    if (Input::GetKeyPress(VK_DOWN))
+    {
+        m_CameraPitch -= pitchStep;
+    }
+
+    const float maxPitch = 1.2f;
+    const float minPitch = -1.2f;
+
+    if (m_CameraPitch > maxPitch) m_CameraPitch = maxPitch;
+    if (m_CameraPitch < minPitch) m_CameraPitch = minPitch;
 }
 
 void Camera::SetCamera(int mode)
 {
-	//3D
-	if (mode == 0)
-	{
-		// カメラの向きからターゲット位置を計算
-		Vector3 forward;
-		forward.x = cosf(m_CameraPitch) * sinf(m_CameraDirection);
-		forward.y = -sinf(m_CameraPitch);
-		forward.z = cosf(m_CameraPitch) * cosf(m_CameraDirection);
-		forward.Normalize();
+    if (mode == 0)
+    {
+        Vector3 forward = GetForward();
 
-		m_Target = m_Position + forward;
+        m_Target = m_Position + forward;
 
-		Vector3 up = Vector3(0.0f, 1.0f, 0.0f);
-		m_ViewMatrix = DirectX::XMMatrixLookAtLH(m_Position, m_Target, up);
+        Vector3 up = Vector3(0.0f, 1.0f, 0.0f);
 
-		Renderer::SetViewMatrix(&m_ViewMatrix);
+        m_ViewMatrix =
+            DirectX::XMMatrixLookAtLH(
+                m_Position,
+                m_Target,
+                up
+            );
 
-		constexpr float fieldOfView = DirectX::XMConvertToRadians(45.0f);
-		float aspectRatio = static_cast<float>(Application::GetWidth()) / static_cast<float>(Application::GetHeight());
-		float nearPlane = 1.0f;
-		float farPlane = 1000.0f;
+        Renderer::SetViewMatrix(&m_ViewMatrix);
 
-		Matrix projectionMatrix = DirectX::XMMatrixPerspectiveFovLH(fieldOfView, aspectRatio, nearPlane, farPlane);
-		Renderer::SetProjectionMatrix(&projectionMatrix);
-	}
-	/*
-	//2D
-	else if (mode == 1)
-	{
-		Vector3 pos = { 0.0f,0.0f,-10.0f };
-		Vector3 tgt = { 0.0f,0.0f,1.0f };
-		Vector3 up = Vector3(0.0f, 1.0f, 0.0f);
-		m_ViewMatrix = DirectX::XMMatrixLookAtLH(pos, tgt, up);
-		Renderer::SetViewMatrix(&m_ViewMatrix);
+        constexpr float fieldOfView =
+            DirectX::XMConvertToRadians(45.0f);
 
-		float nearPlane = 1.0f;
-		float farPlane = 1000.0f;
-		Matrix projectionMatrix = DirectX::XMMatrixOrthographicLH(
-			static_cast<float>(Application::GetWidth()),
-			static_cast<float>(Application::GetHeight()),
-			nearPlane, farPlane);
-		projectionMatrix = DirectX::XMMatrixTranspose(projectionMatrix);
-		Renderer::SetProjectionMatrix(&projectionMatrix);
-	}*/
+        float aspectRatio =
+            static_cast<float>(Application::GetWidth()) /
+            static_cast<float>(Application::GetHeight());
+
+        float nearPlane = 1.0f;
+        float farPlane = 1000.0f;
+
+        Matrix projectionMatrix =
+            DirectX::XMMatrixPerspectiveFovLH(
+                fieldOfView,
+                aspectRatio,
+                nearPlane,
+                farPlane
+            );
+
+        Renderer::SetProjectionMatrix(&projectionMatrix);
+    }
 }
 
-void Camera::Uninit() 
+void Camera::Uninit()
 {
-	// マウスカーソルを再表示
-	ShowCursor(TRUE);
+    ShowCursor(TRUE);
 }
 
-void Camera::SetTarget(Vector3 target) { m_Target = target; }
+void Camera::SetTarget(Vector3 target)
+{
+    m_Target = target;
+}
+
+Vector3 Camera::GetForward() const
+{
+    float cp = cosf(m_CameraPitch);
+
+    Vector3 forward(
+        sinf(m_CameraDirection) * cp,
+        sinf(m_CameraPitch),
+        cosf(m_CameraDirection) * cp
+    );
+
+    forward.Normalize();
+
+    return forward;
+}
--- Job_application_work_horror/Camera.h
+++ Job_application_work_horror/Camera.h
@@ -1,30 +1,58 @@
 ﻿#pragma once
 
-#include	<SimpleMath.h>
+#include <Windows.h>
+#include <SimpleMath.h>
 
-class Camera {
+class Camera
+{
 private:
-	POINT m_LastMousePos{};
-	bool m_FirstMouse = true;
-	DirectX::SimpleMath::Vector3	m_Position = DirectX::SimpleMath::Vector3(0.0f, 0.0f, 0.0f);
-	DirectX::SimpleMath::Vector3	m_Rotation = DirectX::SimpleMath::Vector3(0.0f, 0.0f, 0.0f);
-	DirectX::SimpleMath::Vector3	m_Scale = DirectX::SimpleMath::Vector3(1.0f, 1.0f, 1.0f);
+    POINT m_LastMousePos{};
+    bool m_FirstMouse = true;
 
-	DirectX::SimpleMath::Vector3	m_Target{};
-	DirectX::SimpleMath::Matrix		m_ViewMatrix{};
+    bool m_MouseLookEnable = true;
 
-	float m_CameraDirection = 0; // �J�����̕����iYaw�j
-	float m_CameraPitch = 0.0f;  // �㉺�p�iPitch�j
+    DirectX::SimpleMath::Vector3 m_Position =
+        DirectX::SimpleMath::Vector3(0.0f, 0.0f, 0.0f);
+
+    DirectX::SimpleMath::Vector3 m_Rotation =
+        DirectX::SimpleMath::Vector3(0.0f, 0.0f, 0.0f);
+
+    DirectX::SimpleMath::Vector3 m_Scale =
+        DirectX::SimpleMath::Vector3(1.0f, 1.0f, 1.0f);
+
+    DirectX::SimpleMath::Vector3 m_Target{};
+    DirectX::SimpleMath::Matrix m_ViewMatrix{};
+
+    float m_CameraDirection = 0.0f;
+    float m_CameraPitch = 0.0f;
 
 public:
-	void Init();
-	void Update();
-	void UpdateMouseLook();
-	void SetCamera(int mode = 0);
-	void Uninit();
-	void SetTarget(DirectX::SimpleMath::Vector3 target);
+    void Init();
+    void Update();
+    void SetCamera(int mode = 0);
+    void Uninit();
 
-	void SetPosition(DirectX::SimpleMath::Vector3 pos) { m_Position = pos; }
-	float GetCameraDirection() const { return m_CameraDirection; }
-	float GetCameraPitch() const { return m_CameraPitch; } // ���ǉ�
+    void SetTarget(DirectX::SimpleMath::Vector3 target);
+
+    void SetPosition(DirectX::SimpleMath::Vector3 pos)
+    {
+        m_Position = pos;
+    }
+
+    DirectX::SimpleMath::Vector3 GetPosition() const
+    {
+        return m_Position;
+    }
+
+    float GetCameraDirection() const
+    {
+        return m_CameraDirection;
+    }
+
+    float GetCameraPitch() const
+    {
+        return m_CameraPitch;
+    }
+
+    DirectX::SimpleMath::Vector3 GetForward() const;
 };
